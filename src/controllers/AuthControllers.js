fcdc2dc64607cb34892c57c837655b99c0e87dc6
maulanas13 <maulana.septanto@hotmail.com>
2021-11-11 01:39:04 +0700

    TDL-5, developed register controllers v1.0

--- src/controllers/AuthControllers.js
+++ src/controllers/AuthControllers.js
@@ -1,5 +1,5 @@
 const {mySqlDb} = require("../connections");
-const { hashPass, createToken } = require("../helpers");
+const { hashPass, createToken, transporter } = require("../helpers");
 const { createTokenEmailVerified, createTokenAccess } = createToken;
 const handlebars = require("handlebars");
 const path = require("path");
@@ -16,6 +16,8 @@ module.exports = {
             if (dataUser.length) {
                 throw {message: "Username sudah terdaftar"}
             }
+
+            // Masukin data regis user ke SQL
             sql = "INSERT INTO user SET ?"
             let dataInsert = {
                 email,
@@ -35,20 +37,36 @@ module.exports = {
                 username: userData[0].username,
                 role_id: userData[0].role.id,
             };
+
+            // Utk melepaskan koneksi dari pool, kemudian lanjut dgn kirim email verifikasi
             conn.release();
+
+            // Bikin token email verifikasi & token akses
             const emailToken = createTokenEmailVerified(dataToken);
             const accessToken = createTokenAccess(dataToken);
+
+            // Ambil template email verifikasi & kirim
             let filepath = path.resolve(__dirname, "../template/VerifikasiEmail.html");
             let htmlString = fs.readFileSync(filepath, "utf-8");
             const template = handlebars.compile(htmlString);
             const htmlToEmail = template({
                 name: username,
                 token: emailToken,
-            })
+            });
+            transporter.sendMail({
+                from: "Admin <tesproduk13@gmail.com>",
+                to: email,
+                subject: "Registration Verification Email",
+                html: htmlToEmail,
+            });
+
+            // Simpan token pada header
+            res.set("x-token-access", accessToken);
+            return res.status(200).send({...userData[0]});
         } catch (error) {
             conn.release();
             console.log(error);
             return res.status(500).send({message: error.message || "Server error"});
-        }
+        };
     }
 }
